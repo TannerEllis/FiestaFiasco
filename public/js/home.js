b4988b360fd722b26a82e9a51e8e13f38e84c35d
Brance Boren <branceboren@gmail.com>
2017-03-15 23:15:22 -0600

    Places search and marker creation complete

--- public/js/home.js
+++ public/js/home.js
@@ -1,28 +1,38 @@
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 var map;
 var infowindow;
+var lat;
+var lng;
+var mileRadius = 10;
 
 function initMap() {
-  var pyrmont = {lat: -33.867, lng: 151.195};
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+      var userLocation = {lat: lat, lng: lng};
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: pyrmont,
-    zoom: 15
-  });
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: userLocation,
+        zoom: 14
+      });
 
-  infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: pyrmont,
-    radius: 500,
-    type: ['store']
-  }, callback);
+      infowindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: userLocation,
+        radius: 1610 * mileRadius,
+        type: 'food'
+      }, callback);
+    });
+  } else {
+    console.log('geolocation NOT available ;(');
+  }
 }
 
-function callback(results, status) {
+function callback(results, status, pagination) {
+  setTimeout(function() {
+    pagination.nextPage()
+  }, 3000);
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
@@ -31,7 +41,6 @@ function callback(results, status) {
 }
 
 function createMarker(place) {
-  var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
     position: place.geometry.location
